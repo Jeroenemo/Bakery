842104e2e940073745ea1dbc407825fdbda2b2a1
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-02-26 09:45:38 -0800

    add bread cost calc

--- Bakery/Models/Bakery.cs
+++ Bakery/Models/Bakery.cs
@@ -11,7 +11,16 @@ namespace Bakery.Models
     }
     public int ClaculatePrice()
     {
-      
+      int updatedQuantity = Quantity;
+      for (int i = 0; i < Quantity; i++)
+      {
+        if (i % 2 == 0)
+        {
+          updatedQuantity ++;
+        }
+        total = Quantity * UnitPrice;
+      }
+      return ("You got {0} loaves for ${1} -- {2} loaves where free!", updatedQuantity, total, updatedQuantity - Quantity);
     }
   }
 
