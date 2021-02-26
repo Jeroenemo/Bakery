909cb83bfe41c56710366d677b17c97fcff3fb0f
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-02-26 11:48:02 -0800

    pass spec for set pastry quantity

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -14,5 +14,15 @@ namespace Bakery.Tests
       Pastry newPastry = new Pastry(3);
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
+    [TestMethod]
+    public void SetQuantity_ReturnsQuantity_Int()
+    {
+      int quantity = 8;
+
+      Pastry newPastry = new Pastry(8);
+      int result = newPastry.Quantity;
+
+      Assert.AreEqual(quantity, result);
+    }
   }
 }
