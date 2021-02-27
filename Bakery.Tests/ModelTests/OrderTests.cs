f36e7b2da9b2109d773ae3ba0ec435f8b8723bf3
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-02-26 16:15:14 -0800

    spec for order object type

--- Bakery.Tests/ModelTests/OrderTests.cs
+++ Bakery.Tests/ModelTests/OrderTests.cs
@@ -6,13 +6,31 @@ using System;
 namespace Bakery.Tests
 {
   [TestClass]
-  public class OrderTests
+  public class OrderTests : IDisposable
   {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("Bread",(3,5));
+      Order newOrder = new Order("Bread",(3,15));
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void GetOrderType_ReturnsCurrentOrderType_String()
+    {
+      (int, int) currentOrder = (3, 5);
+      string orderType = "Pastries";
+
+      Order newOrder = new Order(orderType, currentOrder);
+      string result = newOrder.Type;
+      
+      Assert.AreEqual(result, orderType);
+    }
+
   }
 }
--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -6,8 +6,8 @@ namespace Bakery.Models
 {
   public  class Order
   {
-    public (int, int) CurrentOrder { get; set; }
-    public string Type { get; set; }
+    public (int, int) CurrentOrder { get; }
+    public string Type { get; }
     private static List<Order> _order = new List<Order> {};
 
     public Order(string type, (int, int) order)
@@ -23,5 +23,9 @@ namespace Bakery.Models
       return _order;
     }
     
+    public static void ClearAll()
+    {
+      _order.Clear();
+    }
   }
 }
