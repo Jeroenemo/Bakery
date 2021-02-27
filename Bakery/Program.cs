57a90d97c01f7622b26c32cbcae0c0f6aedd7fcc
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-02-26 20:17:05 -0800

    Refactor user prompt

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -14,7 +14,7 @@ namespace Bakery
       switch (userInput)
       {
         case "B":
-          Console.WriteLine("It's $5 per loaf, Buy 2 get 1 Free! How many loaves would you like?");
+          Console.WriteLine("It's $5 per loaf, Buy 2 get 1 Free Special! How many loaves would you like?");
           int breadQuantity = int.Parse(Console.ReadLine());
           Bread newBread = new Bread(breadQuantity);
           (int,int) breadResult = newBread.ClaculatePrice();
@@ -23,7 +23,7 @@ namespace Bakery
           break;
 
         case "P":
-          Console.WriteLine("It's $2 per pastry, 3 for $5! How many pastries would you like?");
+          Console.WriteLine("It's $2 per pastry, or 3 for $5! How many pastries would you like?");
           int pastryQuantity = int.Parse(Console.ReadLine());
           Pastry newPastry = new Pastry(pastryQuantity);
           (int, int) pastryResult = newPastry.ClaculatePrice();
@@ -31,6 +31,7 @@ namespace Bakery
           Main();
           break;
         case "C":
+
           List<Order> allOrders = Order.GetOrder();
           int grandTotal = 0;
           foreach (Order thisOrder in allOrders)
@@ -38,7 +39,6 @@ namespace Bakery
             grandTotal += thisOrder.CurrentOrder.Item1;
             Console.WriteLine("----------------------");
             Console.WriteLine("{0}: {1} for ${2}", thisOrder.Type, thisOrder.CurrentOrder.Item2, thisOrder.CurrentOrder.Item1);
-            // Console.WriteLine(thisOrder.CurrentOrder.Item1 + thisOrder.Type);
           }
           Console.WriteLine("----------------------");
           Console.WriteLine("Your grand total is ${0}", grandTotal);
